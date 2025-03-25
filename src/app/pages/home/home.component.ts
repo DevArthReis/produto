6719724778e3f365b0bacf54c78f5592ae59bee2
arthur albuquerque <arthuralbuquerque.hsh@gmail.com>
2025-03-25 15:12:30 -0300

    Finalizado e corrijido

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+
+  featuredGames = [
+    { name: 'Jogo A', image: 'assets/images/gameA.jpg', description: 'Jogo incrível para todas as idades!' },
+    { name: 'Jogo B', image: 'assets/images/gameB.jpg', description: 'Aventura épica em um mundo aberto.' },
+    { name: 'Jogo C', image: 'assets/images/gameC.jpg', description: 'Desafios emocionantes e gráficos fantásticos!' }
+  ];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
 
 }
